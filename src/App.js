b43553f275a982c1cbcc21445949e8c09a042b97
Ned Arctic <nedarctic@gmail.com>
2024-03-13 23:59:23 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  console.log('reducer', state, action);
+
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "RESET" });
+
+const App = () => (
+  <Provider store={store}>
+    <Counter/>
+  </Provider>
+);
 
 export default App;
